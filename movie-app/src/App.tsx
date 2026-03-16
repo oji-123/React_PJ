11d30d7f74ae00c28c17fee4f4bc00b9de32cd9a
oji-123 <om1997518@gmail.com>
2026-03-16 13:32:35 +0900

    TypeScriptでの型定義

--- movie-app/src/App.tsx
+++ movie-app/src/App.tsx
@@ -1,6 +1,32 @@
 import './App.css'
 import { useEffect, useState } from "react"; // 非同期処理を実行するためにuseEffectを追加
 
+// 映画情報の一覧の型定義
+type Movie = {
+  id: string;
+  original_title: string;
+  poster_path: string;
+  overview: string;
+};
+
+// APIレスポンスの型定義
+type MovieJson = {
+  adult: boolean;
+  backdrop_path: string | null;
+  genre_ids: number[];
+  id: string;
+  original_language: string;
+  original_title: string;
+  overview: string;
+  popularity: number;
+  poster_path: string | null;
+  release_date: string;
+  title: string;
+  video: boolean;
+  vote_average: number;
+  vote_count: number;
+};
+
 function App() {
 
   const defaultMovieList = [
@@ -50,7 +76,7 @@ function App() {
   // keybordは状態を保持する変数、setKeywordは状態の変数を更新するためのメソッド名、useStateはReact Hooksの一種
   const [keyword, setKeyword] = useState("");
   // 映画の検索結果を保持するための変数
-  const [movieList, setMovieList] = useState<any[]>([]);
+  const [movieList, setMovieList] = useState<Movie[]>([]);
 
   useEffect(() => {
     fetchMovieList();
