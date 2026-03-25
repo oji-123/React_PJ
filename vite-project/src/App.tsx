18aa11bc9f71670ff04c2c1036ea61eaa704aa83
oji-123 <om1997518@gmail.com>
2026-03-25 10:08:02 +0900

    One-way Data Flowに基づいて、修正

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -1,11 +1,25 @@
+import { useState } from "react";
 import Title from "./components/Title"
 import Form from "./components/Form"
+import Result from "./components/Result"
 
 const App = () => {
+
+  const API_KEY = import.meta.env.VITE_WEATHER_API_KEY;
+
+  const [city, setCity] = useState<string>("");
+
+  const getWeather = (e: any) => {
+    e.preventDefault();
+    fetch(`https://api.weatherapi.com/v1/current.json?key=${API_KEY}&q=London&aqi=no`)
+      .then(res => res.json())
+      .then(data => console.log(data))
+  }
+
   return (
     <div>
       <Title />
-      <Form />
+      <Form setCity={setCity} getWeather={getWeather} />
       <Result />
     </div>
   )
--- vite-project/src/components/Form.tsx
+++ vite-project/src/components/Form.tsx
@@ -1,22 +1,14 @@
-import { useState } from "react";
+type FormProps = {
+    setCity: React.Dispatch<React.SetStateAction<string>>,
+    getWeather: (e: any) => void
+}
 
-const Form = () => {
-
-    const API_KEY = import.meta.env.VITE_WEATHER_API_KEY;
-
-    const [city, setCity] = useState<string>("");
-
-    const getWeather = (e: any) => {
-        e.preventDefault();
-        fetch(`https://api.weatherapi.com/v1/current.json?key=${API_KEY}&q=London&aqi=no`)
-            .then(res => res.json())
-            .then(data => console.log(data))
-    }
+const Form = (props: FormProps) => {
 
         return (
             <form>
-                <input type="text" name="city" placeholder="都市名" onChange={(e) => setCity(e.target.value)} />
-                <button type="submit" onClick={getWeather}>Get Weather</button>
+                <input type="text" name="city" placeholder="都市名" onChange={(e) => props.setCity(e.target.value)} />
+                <button type="submit" onClick={props.getWeather}>Get Weather</button>
             </form>
         )
     }
