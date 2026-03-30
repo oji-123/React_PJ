28df81882e405393012f806438cac4ea3a8416d3
oji-123 <om1997518@gmail.com>
2026-03-30 11:51:40 +0900

    ページのタイトルを変更

--- vite-project/src/components/Result.tsx
+++ vite-project/src/components/Result.tsx
@@ -10,17 +10,17 @@ type ResultProps = {
 
 const Result = (props: ResultProps) => {
     return (
-        <div>
+        <>
             {props.result.country &&
-                <div>
+                <>
                     <div className="results-country">{props.result.country}</div>
                     <div className="results-city">{props.result.cityName}</div>
                     <div className="results-temp">{props.result.templature}<span>°C</span></div>
                     <div className="results-condition">{props.result.conditionText}</div>
                     <img src={props.result.icon} alt="icon" />
-                </div>
+                </>
             }
-        </div>
+        </>
     )
 }
 
